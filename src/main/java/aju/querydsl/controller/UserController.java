04a73bdd163945141b0a1b35ec90337d2d2752e4
JeongIn <devforin@gmail.com>
2022-10-04 16:45:36 +0900

    querydsl Create 초기화

--- src/main/java/aju/querydsl/controller/UserController.java
+++ src/main/java/aju/querydsl/controller/UserController.java
@@ -3,8 +3,6 @@ package aju.querydsl.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import aju.querydsl.entity.User;
 import aju.querydsl.repository.UserRepository;
 import aju.querydsl.repository.UserRepositorySupport;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 public class UserController {
@@ -36,11 +33,8 @@ public class UserController {
 	
 	// User Create 
 	@PostMapping("/Users")
-	public ResponseEntity<?> save(@RequestBody User user) {
-		if(userRepositorySupport.findById(user.getId()) == null){
-			ResponseEntity.status(HttpStatus.OK).body(userRepository.save(user));			
-		} 
-		return ResponseEntity.notFound().build();
+	public void save(User user) {
+		userRepository.save(user);
 	}
 	
 	// User find by id
