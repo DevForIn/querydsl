c10a5c1a667968cbbe4aa2a9dd6832ae74d366ce
JeongIn <devforin@gmail.com>
2022-10-04 16:41:01 +0900

    querydsl Create 예외처리

--- src/main/java/aju/querydsl/controller/UserController.java
+++ src/main/java/aju/querydsl/controller/UserController.java
@@ -3,6 +3,8 @@ package aju.querydsl.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import aju.querydsl.entity.User;
 import aju.querydsl.repository.UserRepository;
 import aju.querydsl.repository.UserRepositorySupport;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 public class UserController {
@@ -33,11 +36,11 @@ public class UserController {
 	
 	// User Create 
 	@PostMapping("/Users")
-	public void save(User user) {
-		if(!userRepositorySupport.findById(user.getId()).equals(null)){
-			System.out.println("데이터 입력 오류");
-		}
-		else userRepository.save(user);
+	public ResponseEntity<?> save(@RequestBody User user) {
+		if(userRepositorySupport.findById(user.getId()) == null){
+			ResponseEntity.status(HttpStatus.OK).body(userRepository.save(user));			
+		} 
+		return ResponseEntity.notFound().build();
 	}
 	
 	// User find by id
