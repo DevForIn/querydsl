04e9fa8ad8e32b88fe0c5119fda8f15689bd23d9
JeongIn <devforin@gmail.com>
2022-10-12 17:22:43 +0900

    Controller 나눔  yaml 파일 확인 필요 (MySql 연동 상태 )

--- src/test/java/aju/querydsl/QuerydslApplicationTests.java
+++ src/test/java/aju/querydsl/QuerydslApplicationTests.java
@@ -3,15 +3,26 @@ package aju.querydsl;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import java.util.Optional;
+import java.util.Scanner;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import aju.querydsl.entity.User;
+import aju.querydsl.repository.UserRepository;
+import aju.querydsl.service.MainServiceImpl;
 
 @SpringBootTest
 class QuerydslApplicationTests {
 
+	@Autowired
+	MainServiceImpl msi;
+	
 	@Test
-	void 테스트() {
+	void 테스트Assert() {
 		System.out.println("test");
 		boolean test = true;
 		int a = 5;
@@ -25,5 +36,20 @@ class QuerydslApplicationTests {
 				() -> assertEquals(test, true, () -> "test != ture. 확인필요"),
 				() -> assertNotEquals(a, 3, () -> "a == a. 확인필요")				
 				);
+	}
+	
+	@Test
+	void 회원검색() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("아이디를 입력하세요.");
+		
+		Long id = sc.nextLong();
+		
+		User result = msi.findById(id);
+		
+		System.out.println("------------------------");
+        if(!result.equals(null)){
+            System.out.println(result);
+        }		
 	}
 }
